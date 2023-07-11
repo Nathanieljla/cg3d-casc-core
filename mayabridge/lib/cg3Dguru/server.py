3769e7efae43d9cea89de0e8b2024c6571367201
Nathanieljla <nathanieljla@gmail.com>
2023-07-11 09:36:21 -0500

    revamping folder structure

--- mayabridge/lib/cg3Dguru/server.py
+++ mayabridge/lib/cg3Dguru/server.py
@@ -4,7 +4,12 @@ import socket
 import threading
 
 
-#TODO:  Read this https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
+__SERVER = None
+
+
+#TODO: Read this https://www.geeksforgeeks.org/python-different-ways-to-kill-a-thread/
+#TODO: I should probably change this to a subprocess or multiprocess? https://stackoverflow.com/questions/2629680/deciding-among-subprocess-multiprocessing-and-thread-in-python
+#TODO: Handle killing thread/subprocess if parent process crashes. https://stackoverflow.com/questions/23434842/python-how-to-kill-child-processes-when-parent-dies
 class Server(threading.Thread):
     def __init__(self):
         threading.Thread.__init__(self) 
@@ -13,7 +18,7 @@ class Server(threading.Thread):
         self.backlog = 5
         self.size = 1024
         self.socket = None
-        self.running = False
+        self.running = self.open_socket()
         
     def __del__(self):
         print("Deleting cg3Dguru.com.server")
@@ -37,7 +42,9 @@ class Server(threading.Thread):
             return False
 
     def run(self):
-        self.running = self.open_socket()
+        if not self.running:
+            print("Failed to open port!  Thread failed.")
+            return
 
         while self.running:
             client, address = self.socket.accept()
@@ -53,8 +60,24 @@ class Server(threading.Thread):
 
     def stop(self):
         self.running = False
-        if self.socket:
-            self.socket = None
+        #if self.socket:
+            #self.socket = None
+
+
+
+def start_server():
+    global __SERVER
+    if __SERVER is None:
+        __SERVER = Server()
+        
+    __SERVER.run()
+    return __SERVER.is_alive()
+
+
+def stop_server():
+    if __SERVER is not None and __SERVER.is_alive():
+        __SERVER.stop()
+
 
 
 
